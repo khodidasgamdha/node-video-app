fd1dadd2a22755428170f85f2c7dbfeb8ac457e3
Khodidas <kagamdha@gmail.com>
2023-05-04 14:43:11 +0530

    changes

--- public/index.js
+++ public/index.js
@@ -1,67 +1,78 @@
-const socket = io('/');
+const socket = io("/");
 const peer = new Peer();
+
 let myVideoStream;
 let myId;
-var videoGrid = document.getElementById('videoDiv')
-var myvideo = document.createElement('video');
+let videoGrid = document.getElementById("videoDiv");
+let myvideo = document.createElement("video");
+
 myvideo.muted = true;
-const peerConnections = {}
-navigator.mediaDevices.getUserMedia({
-  video:true,
-  audio:true
-}).then((stream)=>{
-  myVideoStream = stream;
-  addVideo(myvideo , stream);
-  peer.on('call' , call=>{
-    call.answer(stream);
-      const vid = document.createElement('video');
-    call.on('stream' , userStream=>{
-      addVideo(vid , userStream);
-    })
-    call.on('error' , (err)=>{
-      alert(err)
-    })
-    call.on("close", () => {
+
+const peerConnections = {};
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideo(myvideo, stream);
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const vid = document.createElement("video");
+      call.on("stream", (userStream) => {
+        addVideo(vid, userStream);
+      });
+      call.on("error", (err) => {
+        alert(err);
+      });
+      call.on("close", () => {
         console.log(vid);
         vid.remove();
-    })
-    peerConnections[call.peer] = call;
+      });
+      peerConnections[call.peer] = call;
+    });
   })
-}).catch(err=>{
-    alert(err.message)
-})
-peer.on('open' , (id)=>{
+  .catch((err) => {
+    alert(err.message);
+  });
+
+peer.on("open", (id) => {
   myId = id;
-  socket.emit("newUser" , id , roomID);
-})
-peer.on('error' , (err)=>{
+  socket.emit("newUser", id, roomID);
+});
+
+peer.on("error", (err) => {
   alert(err.type);
 });
-socket.on('userJoined' , id=>{
-  console.log("new user joined")
-  const call  = peer.call(id , myVideoStream);
-  const vid = document.createElement('video');
-  call.on('error' , (err)=>{
+
+socket.on("userJoined", (id) => {
+  console.log("new user joined");
+  const call = peer.call(id, myVideoStream);
+  const vid = document.createElement("video");
+  call.on("error", (err) => {
     alert(err);
-  })
-  call.on('stream' , userStream=>{
-    addVideo(vid , userStream);
-  })
-  call.on('close' , ()=>{
+  });
+  call.on("stream", (userStream) => {
+    addVideo(vid, userStream);
+  });
+  call.on("close", () => {
     vid.remove();
-    console.log("user disconect")
-  })
+    console.log("user disconect");
+  });
   peerConnections[id] = call;
-})
-socket.on('userDisconnect' , id=>{
-  if(peerConnections[id]){
+});
+
+socket.on("userDisconnect", (id) => {
+  if (peerConnections[id]) {
     peerConnections[id].close();
   }
-})
-function addVideo(video , stream){
+});
+
+function addVideo(video, stream) {
   video.srcObject = stream;
-  video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
   videoGrid.append(video);
 }
